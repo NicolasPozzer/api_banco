e5a4960df83289fc349aef56f32e34a2f4f0930b
NicolasPozzer <pozzernico@gmail.com>
2023-10-23 06:43:46 -0300

    first commit

---
+++ src/main/java/com/demo/api_banco/ApiBancoApplication.java
@@ -0,0 +1,13 @@
+package com.demo.api_banco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBancoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiBancoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/api_banco/ApiBancoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.api_banco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBancoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
